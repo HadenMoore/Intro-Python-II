698c41166fcb103a8b5476c0cf78dcd903c37225
Haden Moore <hadens.moore@gmail.com>
2020-01-17 10:59:44 -0500

    Updating Rooms and Player movements.

--- src/player.py
+++ src/player.py
@@ -1,9 +1,17 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
-class Player: 
-    """The Main Character or (Hero) for this incredible, slightly disappointing adventure."""
-    def __init__(self, name, location, inventory): 
+class Player:
+
+    def __init__(self, name, starting_room):
         self.name = name
-        self.location = location
-        self.inventory = inventory
+        self.current_room = starting_room
+
+    def travel(self, direction):
+        # Player should be able to move in a direction
+        next_room = self.current_room.get_room_in_direction(direction)
+        if next_room is not None:
+            self.current_room = next_room
+            print(self.current_room)
+        else:
+            print("You cannot move in that direction.")
